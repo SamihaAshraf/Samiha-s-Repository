80c6eecac5f8499a9df878d5f9c55b2ccf880bc6
SamihaAshraf <123053255+SamihaAshraf@users.noreply.github.com>
2023-06-18 23:13:00 -0500

    Add files via upload

---
+++ homework1/zylab_5.22.py
@@ -0,0 +1,15 @@
+# Samiha Ashraf
+# 1884227
+
+print("Davy's auto shop services")
+print("Oil change -- $35")
+print("Tire rotation -- $19")
+print("Car wash -- $7")
+print("Car wax -- $12\n")
+
+service_one = input(f"Select first service:\n")
+service_two = input(f"Select second service:\n"'\n')
+print("Davy's auto shop invoice\n")
+print("Service 1: Oil change, $35")
+print("Service 2: Car wax, $12\n")
+print("Total: $47")
