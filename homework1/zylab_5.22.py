2e20d8aec95f40dc4d84e61dc55fb269ff722a89
SamihaAshraf <123053255+SamihaAshraf@users.noreply.github.com>
2023-06-18 23:32:45 -0500

    Update zylab_5.22.py

--- homework1/zylab_5.22.py
+++ homework1/zylab_5.22.py
@@ -5,11 +5,37 @@ print("Davy's auto shop services")
 print("Oil change -- $35")
 print("Tire rotation -- $19")
 print("Car wash -- $7")
-print("Car wax -- $12\n")
+print("Car wax -- $12")
 
-service_one = input(f"Select first service:\n")
-service_two = input(f"Select second service:\n"'\n')
-print("Davy's auto shop invoice\n")
-print("Service 1: Oil change, $35")
-print("Service 2: Car wax, $12\n")
-print("Total: $47")
+# Prompt the user to select the first service
+print("\nSelect first service:")
+first_service = input()
+
+# Prompt the user to select the second service
+print("Select second service:")
+second_service = input()
+
+# Output an invoice for the services selected
+service_cost = {
+    "Oil change": 35,
+    "Tire rotation": 19,
+    "Car wash": 7,
+    "Car wax": 12
+}
+
+# Calculate the total cost
+total_cost = service_cost.get(first_service, 0) + service_cost.get(second_service, 0)
+
+# Extend the program to allow the user to enter a dash (-), which indicates no service.
+print("\nDavy's auto shop invoice\n")
+if first_service == "-":
+    print("Service 1: No service")
+else:
+    print("Service 1: {}, ${}".format(first_service, service_cost.get(first_service, 0)))
+
+if second_service == "-":
+    print("Service 2: No service")
+else:
+    print("Service 2: {}, ${}".format(second_service, service_cost.get(second_service, 0)))
+
+print("\nTotal: ${}".format(total_cost))
